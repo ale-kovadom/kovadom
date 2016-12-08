9d7a0d39e6aa681874c06d23c6d5132fc7a2e9f8
Arnaud Lescaroux <arnaud.lescaroux@free.fr>
2016-12-08 22:41:36 +0100

    task/135464659/setupenv Change TS/JS file structure

--- src/main/frontend/gulpfile.js
+++ src/main/frontend/gulpfile.js
@@ -5,10 +5,10 @@ var plugins = require('gulp-load-plugins')();
 
 var paths = { // keep in mind we are in frontend folder for paths
     src: {
-        ts: './ts/**/*.ts'
+        ts: './app/**/*.ts'
     },
     dest: {
-        js: './ts/build/js'
+        js: './app-out'
     }
 };
 
@@ -23,7 +23,7 @@ gulp.task('ts', function () {
     //     base: '/',
     //     html: true,
     //     css: false,
-    //     jade: false,
+    //     jade: false,                 
     //     target: 'es5',
     //     useRelativePaths: true
     // }))
@@ -38,6 +38,14 @@ gulp.task('ts', function () {
         // .pipe(sourcemaps.init({
         //     loadMaps: true
         // }))
+        .pipe(plugins.inlineNg2Template({
+            base: '/',
+            html: true,
+            css: false,
+            jade: false,
+            target: 'es5',
+            useRelativePaths: true
+        }))
         .pipe(ts(tsProject))
         //.pipe(sourcemaps.write('.'))
         .pipe(plugins.uglify())
