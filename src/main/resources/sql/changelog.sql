b996113710996f85c6757be04599f877f1549867
Arnaud Lescaroux <arnaud.lescaroux@free.fr>
2016-12-23 21:12:44 +0100

    task/135464659/setupenv
    - add liquibase

---
+++ src/main/resources/sql/changelog.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+
+--changeset alescaroux:1
+create table if not exists activity (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  code VARCHAR(7) NOT NULL UNIQUE,
+  description VARCHAR(100) NOT NULL,
+  label VARCHAR(100) NOT NULL,
+  created_date TIMESTAMP NOT NULL DEFAULT NOW(),
+  modified_date TIMESTAMP NOT NULL DEFAULT NOW()
+);
