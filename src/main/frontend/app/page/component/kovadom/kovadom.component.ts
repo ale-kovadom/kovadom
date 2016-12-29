80ffd8e7cc503c85cd0fba4ee345e1c7998fdb97
Arnaud Lescaroux <arnaud.lescaroux@free.fr>
2016-12-29 10:40:48 +0100

    task/136754807/user-friendly-navigation
    Scroll to the top of the page during navigation

--- src/main/frontend/app/page/component/kovadom/kovadom.component.ts
+++ src/main/frontend/app/page/component/kovadom/kovadom.component.ts
@@ -1,10 +1,34 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
+import {Subscription} from "rxjs/Subscription";
+import 'rxjs/add/operator/filter';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'kovadom.html',
-    styleUrls: [ 'kovadom.css' ]
+    styleUrls: ['kovadom.css']
 })
-export class KovadomComponent {
+export class KovadomComponent implements OnInit {
+
+    private routerSubscription:Subscription;
+
+    constructor(private router:Router) {
+    }
+
+    ngOnInit() {
+        /*
+         * Scroll to the top of the page when user navigates from "page" to "page". 
+         */
+        this.routerSubscription = this.router.events
+            .filter(event => event instanceof NavigationEnd)
+            .subscribe(event => {
+                document.body.scrollTop = 0;
+            });
+    }
+
+    ngOnDestroy() {
+        this.routerSubscription.unsubscribe();
+    }
+
 
 }
