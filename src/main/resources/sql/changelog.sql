91e583fc07755e24ac09f5fc16c7f94ad868e0a1
Arnaud Lescaroux <arnaud.lescaroux@free.fr>
2016-12-29 10:38:09 +0100

    task/134699277/brands-by-activity
    - Add sql scripts

--- src/main/resources/sql/changelog.sql
+++ src/main/resources/sql/changelog.sql
@@ -13,3 +13,24 @@ create table if not exists activity (
 --changeset alescaroux:2
 ALTER TABLE activity MODIFY code VARCHAR(20);
 ALTER TABLE activity MODIFY id INT UNSIGNED;
+
+--changeset alescaroux:3
+create table if not exists brand (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  code VARCHAR(20) NOT NULL UNIQUE,
+  description VARCHAR(100) NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  catch_words VARCHAR(200) NOT NULL,
+  created_date TIMESTAMP NOT NULL DEFAULT NOW(),
+  modified_date TIMESTAMP NOT NULL DEFAULT NOW()
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE if not exists brand_activity (
+  brand_id INT UNSIGNED NOT NULL,
+  activity_id INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`brand_id`,`activity_id`),
+  CONSTRAINT `fk_brandActivity_brandId_brand_id` FOREIGN KEY (`brand_id`) REFERENCES `brand` (`id`),
+  CONSTRAINT `fk_brandActivity_activityId_activity_id` FOREIGN KEY (`activity_id`) REFERENCES `activity` (`id`)
+) DEFAULT CHARSET=utf8;
+
+ALTER TABLE activity CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
