37830593d9dfc11123e2f6dfb686d54945e7de44
Arnaud Lescaroux <arnaud.lescaroux@free.fr>
2017-01-17 20:58:07 +0100

    task/137554469/user-notification (task/136781407/tomcat) - Use tomcat instead of tomEE

--- src/main/java/com.kovadom/domain/sale/service/DefaultSaleEmailService.java
+++ src/main/java/com.kovadom/domain/sale/service/DefaultSaleEmailService.java
@@ -1,32 +1,26 @@
 package com.kovadom.domain.sale.service;
 
+import com.kovadom.domain.host.Host;
 import com.kovadom.domain.sale.Sale;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 import javax.servlet.ServletContext;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Date;
-import java.util.Properties;
+import java.io.InputStream;
+import java.util.Locale;
+
+import static java.lang.String.format;
+import static org.apache.commons.io.IOUtils.toByteArray;
 
 @Service
 public class DefaultSaleEmailService implements SaleEmailService {
@@ -53,136 +47,25 @@ public class DefaultSaleEmailService implements SaleEmailService {
 
     @Override
     public void notifyHost(Sale sale) {
-//        MimeMessagePreparator preparator = mimeMessage -> {
-//            Host host = sale.getHost();
-//            log.debug(format("Sending email to '%s'", host.getEmail()));
-//
-//            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "UTF-8");
-//            message.setTo(host.getEmail());
-//            message.setFrom(from);
-//            message.setSubject(sale.getStatus().name());   //TODO
-//            Context context = new Context();
-//            context.setVariable("sale", sale);
-//            String logoId = "logo-kovadom.png";
-//            context.setVariable("logoId", logoId);
-//            message.setText(templateEngine.process(VERIFY_AVAILABILITY_TEMPLATE, context), true);
-//
-//            InputStream logoInputStream = servletContext.getResourceAsStream("img/kovadom/logo-kovadom.png");
-//            message.addInline(logoId, new ByteArrayResource(toByteArray(logoInputStream)), "image/png");
-//        };
-//
-//        this.mailSender.send(preparator);
+        MimeMessagePreparator preparator = mimeMessage -> {
+            Host host = sale.getHost();
+            log.debug(format("Sending email to '%s'", host.getEmail()));
 
-        try {
-//            Host host = sale.getHost();
-//            final MimeMessage mimeMessage = this.mailSender.createMimeMessage();
-//            mimeMessage.setRecipients(Message.RecipientType.TO,
-//                    InternetAddress.parse(host.getEmail()));
-//            mimeMessage.setFrom(from);
-//            mimeMessage.setSubject(sale.getStatus().name());
-//
-//
-//            MimeMultipart multipart = new MimeMultipart("related");
-//
-//            // first part (the html)
-//            BodyPart messageBodyPart = new MimeBodyPart();
-//            String htmlText = "<H1>Hello</H1><img src=\"cid:image\">";
-//            messageBodyPart.setContent(htmlText, "text/html");
-//            // add it
-//            multipart.addBodyPart(messageBodyPart);
-//
-//            // second part (the image)
-//            messageBodyPart = new MimeBodyPart();
-//            InputStream logoInputStream = servletContext.getResourceAsStream("img/kovadom/logo-kovadom.png");
-//
-//            messageBodyPart.setDataHandler(new DataHandler(new ByteArrayDataSource(logoInputStream, "image/png")));
-//            messageBodyPart.setHeader("Content-ID", "<image>");
-//
-//            // add image to the multipart
-//            multipart.addBodyPart(messageBodyPart);
-//
-//            // put everything together
-//            mimeMessage.setContent(multipart);
-//            //this.mailSender.send(mimeMessage);
-//            Transport.send(mimeMessage);
-            sendEmailWithAttachments("ns0.ovh.net",
-                    "587",
-                    "clement@kovadom.com",
-                    "qwerty123zaq",
-                    "arnaud.lescaroux@gmail.com",
-                    "test",
-                    "ca marche !",
-                    new String[] {"/Users/arnaudlescaroux/Desktop/kovadom/logo/logo-kovadom.png"}
-                    );
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+            message.setTo(host.getEmail());
+            message.setFrom(from);
+            message.setSubject(sale.getStatus().name());   //TODO
+            Context context = new Context();
+            context.setVariable("sale", sale);
+            String logoId = "logo-kovadom.png";
+            context.setVariable("logoId", logoId);
+            message.setText(templateEngine.process(VERIFY_AVAILABILITY_TEMPLATE, context), true);
 
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        }
-    }
-
-    public static void sendEmailWithAttachments(String host, String port,
-                                                final String userName, final String password, String toAddress,
-                                                String subject, String message, String[] attachFiles)
-            throws AddressException, MessagingException {
-        // sets SMTP server properties
-        Properties properties = new Properties();
-        properties.put("mail.smtp.host", host);
-        properties.put("mail.smtp.port", port);
-        properties.put("mail.smtp.auth", "true");
-       // properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.user", userName);
-        properties.put("mail.password", password);
-
-        // creates a new session with an authenticator
-        Authenticator auth = new Authenticator() {
-            public PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(userName, password);
-            }
+            InputStream logoInputStream = servletContext.getResourceAsStream("img/kovadom/logo-kovadom.png");
+            message.addInline(logoId, new ByteArrayResource(toByteArray(logoInputStream)), "image/png");
         };
-        Session session = Session.getInstance(properties, auth);
 
-        // creates a new e-mail message
-        Message msg = new MimeMessage(session);
-
-        msg.setFrom(new InternetAddress(userName));
-        InternetAddress[] toAddresses = { new InternetAddress(toAddress) };
-        msg.setRecipients(Message.RecipientType.TO, toAddresses);
-        msg.setSubject(subject);
-        msg.setSentDate(new Date());
-
-        // creates message part
-        MimeBodyPart messageBodyPart = new MimeBodyPart();
-       // messageBodyPart.setContent(message, "text/html");
-        messageBodyPart.setText(message);
-
-        // creates multi-part
-        Multipart multipart = new MimeMultipart();
-        multipart.addBodyPart(messageBodyPart);
-
-        // adds attachments
-        if (attachFiles != null && attachFiles.length > 0) {
-            for (String filePath : attachFiles) {
-                MimeBodyPart attachPart = new MimeBodyPart();
-
-                try {
-                    File file = new File(filePath);
-                    if(!file.exists()) {
-                        throw new FileNotFoundException();
-                    }
-                    attachPart.attachFile(file);
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-
-                multipart.addBodyPart(attachPart);
-            }
-        }
-
-        // sets the multi-part as e-mail's content
-        msg.setContent(multipart);
-
-        // sends the e-mail
-        Transport.send(msg);
+        this.mailSender.send(preparator);
 
     }
 
