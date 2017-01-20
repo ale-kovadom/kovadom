f1dbf03f66e7adcb726b49c9887aa45b751f0922
Arnaud Lescaroux <arnaud.lescaroux@free.fr>
2017-01-20 21:33:31 +0100

    Use Tomcat instead of TomEE

--- scripts/deployToServer.sh
+++ scripts/deployToServer.sh
@@ -3,7 +3,7 @@
 cd $KOVADOM_HOME
 scp -P 2244 target/kovadom-0.0.1-SNAPSHOT.war root@vps326792.ovh.net:/tmp
 ssh -p 2244 root@vps326792.ovh.net  << EOF
-cd /opt/apache-tomee-webprofile-7.0.2/webapps
+cd /opt/tomcat/webapps
 ../bin/shutdown.sh
 sleep 15
 cp ROOT ROOT_OLD
