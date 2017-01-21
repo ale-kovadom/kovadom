388cf5cb4f32e21c10066b858211b9abd8296cd4
Arnaud Lescaroux <arnaud.lescaroux@free.fr>
2017-01-21 21:17:59 +0100

    task/137554469/user-notification
    - Fix deployment script

--- scripts/deployToServer.sh
+++ scripts/deployToServer.sh
@@ -2,18 +2,20 @@
 
 cd $KOVADOM_HOME
 scp -P 2244 target/kovadom-0.0.1-SNAPSHOT.war root@vps326792.ovh.net:/tmp
-ssh -p 2244 root@vps326792.ovh.net  << EOF
+ssh -T -p 2244 root@vps326792.ovh.net  << EOF
 cd /opt/tomcat/webapps
 ../bin/shutdown.sh
 sleep 2
 exit
 sleep 15
-cp ROOT ROOT_OLD
+mv ROOT_OLD ROOT_TO_BE_REMOVED
+cp -R ROOT ROOT_OLD
 rm -rf ROOT
 rm -rf ROOT.war
 cp /tmp/kovadom-*.war ROOT.war
 ../bin/startup.sh
 sleep 2
 exit
+rm -rf ROOT_TO_BE_REMOVED
 rm -rf /tmp/kovadom-*.war
 EOF
