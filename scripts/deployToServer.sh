f5f42f9098bbb16a05224bf56152db47349d72ae
Arnaud Lescaroux <arnaud.lescaroux@free.fr>
2017-01-26 20:57:56 +0100

    task/137179803/ssl
    - Simplify deployment script
    - Add first version of favicon

--- scripts/deployToServer.sh
+++ scripts/deployToServer.sh
@@ -4,9 +4,7 @@ cd $KOVADOM_HOME
 scp -P 2244 target/kovadom-0.0.1-SNAPSHOT.war root@vps326792.ovh.net:/tmp
 ssh -T -p 2244 root@vps326792.ovh.net  << EOF
 cd /opt/tomcat/webapps
-../bin/shutdown.sh
-sleep 2
-exit
+../bin/catalina.sh stop 20 -force
 sleep 15
 mv ../ROOT_OLD ../ROOT_TO_BE_REMOVED
 cp -R ROOT ../ROOT_OLD
@@ -14,8 +12,6 @@ rm -rf ROOT
 rm -rf ROOT.war
 cp /tmp/kovadom-*.war ROOT.war
 ../bin/startup.sh
-sleep 2
-exit
 rm -rf ../ROOT_TO_BE_REMOVED
 rm -rf /tmp/kovadom-*.war
 EOF
